d827466c229b788787f06bf8ddd89c8969c77d5b
jlllk <djapme@gmail.com>
2022-11-03 19:47:08 +0300

    added workflow job tests

--- app/src/urls.py
+++ app/src/urls.py
@@ -1,14 +1,17 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.urls import include, path
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('users.urls')),
-    path('', TemplateView.as_view(template_name='home.html'), name='home_page'),
+    path(
+        '',
+        TemplateView.as_view(template_name='home.html'),
+        name='home_page'
+    ),
 ]
 
 if settings.DEBUG:
