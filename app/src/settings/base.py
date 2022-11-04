9412dc5ffab64659a3191112a1e819ca6e145585
jlllk <djapme@gmail.com>
2022-11-04 18:19:52 +0300

    updated login form. added widgets

--- app/src/settings/base.py
+++ app/src/settings/base.py
@@ -78,4 +78,4 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = 'home'
+LOGIN_REDIRECT_URL = 'home_page'
--- app/users/forms.py
+++ app/users/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
@@ -6,6 +7,19 @@ User = get_user_model()
 
 
 class LoginModelForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget = forms.widgets.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Email',
+            'aria - label': 'Email'
+        })
+        self.fields['password'].widget = forms.widgets.PasswordInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Password',
+            'aria - label': 'Password'
+        })
+
     def clean_username(self):
         email = self.cleaned_data['username']
         if email not in settings.ALLOWED_EMAILS:
