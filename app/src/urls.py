81af62a07660bab2d5f4a0194b35ffad4a81bdc9
jlllk <djapme@gmail.com>
2022-11-04 15:03:32 +0300

    added login and logout standart django views

--- app/src/urls.py
+++ app/src/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('users.urls')),
+    path('', include('users.urls')),
     path(
         '',
         TemplateView.as_view(template_name='home.html'),
--- app/users/urls.py
+++ app/users/urls.py
@@ -1,13 +1,18 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path
 
-from .views import SignUpView
-
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
     path(
         'login/',
-        auth_views.LoginView.as_view(template_name='auth/login.html'),
+        auth_views.LoginView.as_view(
+            template_name='auth/login.html',
+            next_page='home_page',
+        ),
         name='login',
+    ),
+    path(
+        'logout/',
+        auth_views.LogoutView.as_view(next_page='home_page'),
+        name='logout',
     ),
 ]
